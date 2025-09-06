e0a3953dc844af83cacc84eaf967db12e4cb9c06
Lucianne-Kesu <jppajela321@gmail.com>
2025-09-06 22:52:08 +0800

    commit

---
+++ pajela_im102_hw1/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace im102_hw1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            bool isLoop = false;
+            bool isPal = false;
+            int counter = 0;
+            string[] inputString = new string[100];
+            string[] placeHolderString = new string[100];
+            while (!isLoop)
+            {
+               
+                Console.Write("Enter String: ");
+                inputString[counter] = Console.ReadLine();
+                if (inputString[counter] == "*") 
+                {
+                isLoop = false;
+                }
+                char[] tempChars = new char[inputString[counter].Length];
+            string placeHolderLower = inputString[counter].ToLower();
+               
+                for (int i = 0; i <= inputString[counter].Length - 1; i++) 
+                {
+                    int reverseIndex = placeHolderLower.Length - 1 - i;
+                   tempChars[i] = placeHolderLower[reverseIndex];
+
+                }
+
+                for (int i = 0; i <= inputString[counter].Length-1;i++) {
+                    placeHolderString[counter] += tempChars[i];
+                }
+
+                if (placeHolderString[counter] == inputString[counter].ToLower()) {
+                    isPal = true;
+                }
+                else { 
+                
+                }
+                Console.WriteLine("Original String:     " + "Reversed String:     " + "Palindrome?");
+                Console.WriteLine("  " +inputString[counter] + "\t\t\t" + placeHolderString[counter] + "\t\t    " + isPal);
+                counter++;
+                
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
